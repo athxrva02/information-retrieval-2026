0af531ce2623cebf08a764609fdda8d00b66d7e6
atharva <dagaonkaratharva@gmail.com>
2026-03-25 23:22:45 +0100

    Add base paper config

--- run_all_ntd.sh
+++ run_all_ntd.sh
@@ -4,6 +4,7 @@ set -e
 SCRIPTS=experiments/recsys_2025/experiment_scripts
 CONFIGS=$SCRIPTS/ntd_configs
 
+python $SCRIPTS/drdw_ntd_runner.py --config $CONFIGS/drdw_base_paper_config.json
 python $SCRIPTS/drdw_ntd_runner.py --config $CONFIGS/optimal_oracle_pure.json
 python $SCRIPTS/drdw_ntd_runner.py --config $CONFIGS/optimal_discriminative.json
 python $SCRIPTS/drdw_ntd_runner.py --config $CONFIGS/optimal_feasibility_aware.json
